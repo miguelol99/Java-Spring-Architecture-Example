68d6b8a54e3d9fedbcdbe2c30a27d3488ad165d4
Miguel Nuñez Saiz <mnsaiz@outlook.es>
2022-07-20 13:17:35 +0200

    Añadido reposittory al userservice

--- src/main/java/com/mignune/users/usersapp/services/UserService.java
+++ src/main/java/com/mignune/users/usersapp/services/UserService.java
@@ -1,7 +1,13 @@
 package com.mignune.users.usersapp.services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mignune.users.usersapp.repositories.UserRepository;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
     
 }
